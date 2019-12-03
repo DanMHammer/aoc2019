306216f9c8a284bc25eb5d6480853c5b3302a9ef
Dan Hammer <daniel.m.hammer@gmail.com>
2019-12-03 12:17:52 -0600

    still not working

--- src/solutions/day3/main.py
+++ src/solutions/day3/main.py
@@ -17,21 +17,81 @@ def traverse_path(path):
         positions.append((x, y))
     return positions
 
+
+def find_intersections(path1, path2):
+    intersections = []
+    for x in range(0, len(path1) - 1, 2):
+        for y in range(0, len(path2) - 1, 2):
+            res = intersect(path1[x:x+2], path2[y:y+2])
+            if res:
+                intersections.append(res)
+            else:
+                pass
+    return intersections
+
+def intersect(segment1, segment2):
+    line1 = line(segment1[0], segment1[1])
+    line2 = line(segment2[0], segment2[1])
+
+    if not line1 or not line2:
+        return False
+    else:
+        m1, x1 = line1
+        m2, x2 = line2
+
+        if m1 == m2:
+            return False
+        else:
+            Ax, Ay = segment1[0]
+            Bx, By = segment1[1]
+            Cx, Cy = segment2[0]
+            Dx, Dy = segment2[0]
+
+            boundsX1 = (min(Ax, Bx), max(Ax, Bx))
+            boundsY1 = (min(Ay, By), max(Ay, By))
+            boundsX2 = (min(Cx, Dx), max(Cx, Dx))
+            boundsY2 = (min(Cy, Dy), max(Cy, Dy))
+
+            x = (b2 - b1)/(m1 - m2)
+            y = m1 * x + b1
+
+            if (boundsX1[0] <= x <= boundsX1[1] and
+                boundsX2[0] <= x <= boundsX2[1] and
+                boundsY1[0] <= y <= boundsY1[1] and
+                boundsY2[0] <= y <= boundsY2[1]):
+                return x, y
+            else:
+                return False
+
+
+def line(point1, point2):
+    print(f"Point 1 {point1}")
+    print(f"Point 2 {point2}")
+    x1, y1 = point1
+    x2, y2 = point2
+
+    if x2 - x1 == 0:
+        return False
+    else:
+        slope = (y2 - y1)/(x2 - x1)
+        intercept = y1 - slope * x1
+        return slope, intercept
+
+
 if __name__ == "__main__":
     paths = pd.read_csv("./input.csv", header=None)
     wire1_path = paths.iloc[0].values
     wire2_path = paths.iloc[1].values
 
-    wire1_positions = set(traverse_path(wire1_path))
-    wire2_positions = set(traverse_path(wire2_path))
+    wire1_positions = traverse_path(wire1_path)
+    wire2_positions = traverse_path(wire2_path)
 
-    intersections = wire1_positions.intersection(wire2_positions)
+    intersections = [find_intersections(wire1_positions, wire2_positions)]
+    #distances = [abs(x) + abs(y) for x, y in intersections]
 
-    distances = []
-
-    for i in intersections:
-        a, b = i
-        distances.append(abs(a) + abs(b))
 
     # print(f"Part One: {min(distances)}")
     print(wire1_positions)
+    print(wire2_positions)
+    print(intersections)
+    #print(distances)
