adb945015741717186c6a1883ddc24f605854c78
Dan Hammer <daniel.m.hammer@gmail.com>
2019-12-07 22:20:15 -0600

    Day 6 Part One Done

--- src/solutions/day6/main.py
+++ src/solutions/day6/main.py
@@ -1,35 +1,64 @@
+class Body:
+    def __init__(self, name):
+        self.name = name
+        self.children = set()
+        self.parent = None
+
+    def add_child(self, child_name):
+        self.children.add(child_name)
+
+    def add_parent(self, parent):
+        self.parent = parent
+
+    def direct(self):
+        return len(self.children)
+
+    def indirect(self):
+        if self.name == "COM":
+            return 0
+        node = self.parent
+        distance = 0
+        while node.name != "COM":
+            distance += 1
+            node = node.parent
+        return distance
+
+
 class OrbitMap:
     def __init__(self):
-        self.data = {}
+        self.bodies = dict()
 
-    def add(self, orbit):
-        o = orbit.split(")")
-        center, orbiter = o[0], o[1]
-        if center not in self.data.keys():
-            self.data[center] = [orbiter]
-        else:
-            self.data[center].append(orbiter)
-        if orbiter not in self.data.keys():
-            self.data[orbiter] = []
+    def load_body(self, parent, child):
+        if parent not in self.bodies.keys():
+            self.bodies[parent] = Body(parent)
+        if child not in self.bodies.keys():
+            self.bodies[child] = Body(child)
+        self.bodies[parent].add_child(self.bodies[child])
+        self.bodies[child].add_parent(self.bodies[parent])
 
-    def count(self):
-        indirect, direct = 0, 0
-        for center, orbits in self.data.items():
-            direct += len(orbits)
-            for values in self.data.values():
-                if center in values:
-                    indirect += 1
-        return indirect, direct
+    def direct(self):
+        num = 0
+        for body_name, body_object in self.bodies.items():
+            num += body_object.direct()
+        return num
 
-    def __str__(self):
-        return str(self.data)
+    def indirect(self):
+        num = 0
+        for body_name, body_object in self.bodies.items():
+            num += body_object.indirect()
+        return num
 
 
 if __name__ == "__main__":
     with open("./input.txt", "r") as file:
         Orbits = OrbitMap()
         for line in file:
-            Orbits.add(line.rstrip())
-        indirect, direct = Orbits.count()
-        print(indirect + direct)
-        print(Orbits)
+            line = line.rstrip()
+            parent, child = line.split(")")
+            Orbits.load_body(parent, child)
+        direct = Orbits.direct()
+        indirect = Orbits.indirect()
+        print(f"Direct: {direct}")
+        print(f"Indirect: {indirect}")
+        print(f"Part One: {direct + indirect}")
+
--- src/solutions/day7/main.py
+++ src/solutions/day7/main.py
@@ -1,13 +1,15 @@
 import csv
 from collections import deque
 outputs = []
+from itertools import permutations
 
-def operate(data, pointer, inputs, output):
+
+def operate(data, pointer, inputs):
     commands = data[pointer]
     pointer += 1
     opcode = commands % 100
     parameters = [commands // 100 % 10, commands // 1000 % 10, commands // 10000 % 10]
-    output = output
+    output = False
 
     if opcode == 1:  # add
         if parameters[0] == 0:
@@ -104,13 +106,13 @@ def operate(data, pointer, inputs, output):
     return data, pointer, inputs, output
 
 
-def operation_loop(data, input1, input2):
-    inputs = deque([input1, input2])
+def operation_loop(data, input1):
+    inputs = deque([input1])
     output = 0
     pointer = 0
     while pointer >= 0:
-        data, pointer, inputs, output = operate(data, pointer, inputs, output)
-    return output
+        data, pointer, inputs, output = operate(data, pointer, inputs)
+    return data, output
 
 
 def amplifier(data, phases, i):
@@ -123,11 +125,71 @@ def amplifier(data, phases, i):
             print(phase, output)
             amplifier(data, [x for x in phases if x != phase], output)
 
+
+class Amp:
+    def __init__(self, data, phase):
+        self.data = data
+        self.phase = phase
+        self.out = 0
+        self.con = True
+
+    def run_operations(self, i=self.phase):
+        print(self.data, self.phase, i)
+        self.data, newout = operation_loop(self.data, i)
+        if newout == false:
+            self.con = False
+        else:
+            self.out = newout
+
+    def get_output(self):
+        return self.out, self.con
+
+    def __str__(self):
+        return str(self.phase)
+
+
+class AmpGroup:
+    def __init__(self, phases, data):
+        self.amps = []
+        self.data = data
+        for phase in phases:
+            self.amps.append(Amp(data[:], phase))
+        self.out = 0
+
+    def __str__(self):
+        o = ""
+        for idx, amp in enumerate(self.amps):
+            o += f"Amp {idx}: {amp}\n"
+        return o
+
+    def run(self):
+        while self.amps:
+            for amp in self.amps:
+                amp.run_operations(self.out)
+                self.out, con = amp.get_output()
+                if not con:
+                    self.amps.remove(amp)
+                print(self.out)
+        return self.out
+
+
 if __name__ == "__main__":
     with open('input.csv', 'r') as f:
         reader = csv.reader(f)
         init_data = [int(x) for x in list(reader)[0]]
-        print(init_data)
-        amplifier(init_data, [0, 1, 2, 3, 4], 0)
-        print(outputs)
-        print(f"Answer One: {max(outputs)}")
+        # print(init_data)
+        #
+        # amplifier(init_data, [0, 1, 2, 3, 4], 0)
+        #
+        # print(outputs)
+        # print(f"Answer One: {max(outputs)}")
+
+        outputs = []
+
+        for p in permutations(range(5, 10)):
+            print(list(p))
+            group = AmpGroup(list(p), init_data[:])
+            print(group)
+            outputs.append(group.run())
+
+        print(f"Answer Two: {max(outputs)}")
